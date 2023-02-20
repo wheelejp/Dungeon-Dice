3bea97f385a3a4012e65e92a3665bff093e5978c
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-02-19 21:43:57 -0500

    Initial Commit

---
+++ dungeonDice/dungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  dungeonDiceApp.swift
+//  dungeonDice
+//
+//  Created by Jonathan Wheeler Jr. on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct dungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
